8b4526dfaae614710b58cc65cf78b574607cb2e1
shahincse98 <32039099+shahincse98@users.noreply.github.com>
2019-01-11 08:17:47 +0600

    python3

---
+++ w3resource python-exercises/Conditional Statements and loops/1.py
@@ -0,0 +1,8 @@
+# 1. Write a Python program to find those numbers which are
+# divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
+
+number = []
+for x in range(1500, 2701):
+    if (x%7 == 0) and (x%5 == 0):
+        number.append(str(x))
+print(','.join(number))
